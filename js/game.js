92d2febbf0b59c926d864cb1c73fd65059418913
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-08-21 06:00:33 +0200

    added first js-class

--- js/game.js
+++ js/game.js
@@ -1,16 +1,11 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MoveableObject();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-
-    character.src = '../img/2_character_pepe/2_walk/W-21.png';
-
-    setTimeout( function() {
-        ctx.drawImage(character, 20, 20, 50, 150);      // x-Koordinate, y-Koordinate, Breite, Höhe
-    }, 2000);
-    
-}
+    console.log('My Character is', character);
+        
+}   
