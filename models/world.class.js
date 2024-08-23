7cec5699e78820dcda30754b327a18ef1d81a44d
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-08-23 05:50:37 +0200

    images will be loaded while loading the website

--- models/world.class.js
+++ models/world.class.js
@@ -6,15 +6,28 @@ class World {
         new Chicken(),
         new Chicken(),
     ];
+    canvas;
     ctx;
 
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+
+        /* this.draw();        // Funktion wiederholt sich in Endlosschleife => Computer stürzt vermutlich ab! */
+        
+        // Draw() wird immer wieder aufgerufen => Wiederholungsrate abhängig von der Grafikkarte
+        self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });       
+
     }
 }
