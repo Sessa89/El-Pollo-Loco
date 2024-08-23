25394fa9175a76a209df4b6e60024f4fc699a4a6
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-08-23 17:25:18 +0200

    added sky

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,11 +1,11 @@
 class BackgroundObject extends MoveableObject {
 
     width = 720;
-    height = 400;
+    height = 480;
 
     constructor(imagePath, x, y) {
         super().loadImage(imagePath);
         this.x = x;
-        this.y = 480 - this.height;     // 480 - 400
+        this.y = 480 - this.height;
     }
 }
