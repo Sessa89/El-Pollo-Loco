9a02b52abbfbae0fe5ceba692dd20c817fcd9910
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-08-23 17:00:42 +0200

    added more backgroundobjects

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,9 @@
 class Chicken extends MoveableObject {
     
+    y = 380;
+    height = 50;
+    width = 50;
+
     constructor() {
         super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
