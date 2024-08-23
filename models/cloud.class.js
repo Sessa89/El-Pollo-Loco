22168a2276d718a13e8d369c83f2b2a2322cbc2e
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-08-23 18:22:05 +0200

    added animation to the clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,6 +7,13 @@ class Cloud extends MoveableObject {
         super().loadImage('./img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500;   // Zahl zwischen 0 und 500 => "Math.random()" gibt eine Zahl zwischen "0" und "1" aus
+        this.animate();
     }
 
+    // alle 60 Bilder pro Sekunde wird die x-Koordinate um 0.15 Pixel verringert
+    animate() {
+        setInterval( () => {
+            this.x -= 0.15;
+        }, 1000 / 60);          // 60 fps
+    }
 }
