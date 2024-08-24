d24d97cdc360eee219c71ec06a32695deb629e67
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-08-24 06:49:33 +0200

    added an array called imageCache for all necessary images of an object

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,11 +4,26 @@ class MoveableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = [];
 
     // loadImage('img/test.png')
     loadImage(path) {
         this.img = new Image();     // this.img = document.getElementById('image') <img id="image">
         this.img.src = path;
+    }
+
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+     */
+    loadImages(arr) {                           // Array wird in die Funktion gegeben (Strings/Pfade aber noch nicht wirkliche Bilder)
+        arr.forEach((path) => {                 // Für jedes Element innerhalb des Arrays wird die Schleife ausgeführt (hier: 6x)
+            let img = new Image();              // Variable "img" wird mit neuem Bild angelegt
+            img.src = path;                     // Bild wird in das Image-Object geladen hinein, Bild kann somit in das Canvas eingefügt werden
+            this.imageCache[path] = path;       // imageCache wird geupdated
+        });
+
     }
 
     moveRight() {
