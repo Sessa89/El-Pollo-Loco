ea44308ad7aa570995a78746e6c606d0cec93b63
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-08-25 07:12:12 +0200

    added movement to the character

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MoveableObject {
     height = 280;
     width = 150;
     y = 150;
+    speed = 5;
     IMAGES_WALKING = [
         './img/2_character_pepe/2_walk/W-21.png',
         './img/2_character_pepe/2_walk/W-22.png',
@@ -21,9 +22,22 @@ class Character extends MoveableObject {
     }
 
     animate() {
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+
         setInterval(() => {
 
-            if (this.world.keyboard.RIGHT) {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                // Walk animation
                 let i = this.currentImage % this.IMAGES_WALKING.length; // "%" = Modulo-Operator = Rest; let i = 0 % 6; => 0, Rest 0
                 // i = 0, 1, 2, 3, 4, 5, 0
                 let path = this.IMAGES_WALKING[i];      // Beim ersten Durchlauf: currentImage = 0
