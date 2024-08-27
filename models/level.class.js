9ddc7bce1c86251e8dadce1a460f4ebc77081494
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-08-27 05:59:11 +0200

    changed project structure and added one level

---
+++ models/level.class.js
@@ -0,0 +1,11 @@
+class Level {
+    enemies;
+    clouds;
+    backgroundObjects;
+
+    constructor(e, c, b) {
+        this.enemies = e;
+        this.clouds = c;
+        this.backgroundObjects = b;
+    }
+}
