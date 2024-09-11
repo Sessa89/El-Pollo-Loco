a7eef231b6caf84c7016e87c2e6d6573b7021a6c
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-09-11 06:46:46 +0200

    changed starting point of the chickens

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -12,7 +12,7 @@ class Chicken extends MoveableObject {
         super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
 
-        this.x = 200 + Math.random() * 500;     // Zahl zwischen 200 und 700 => "Math.random()" gibt eine Zahl zwischen "0" und "1" aus
+        this.x = 350 + Math.random() * 500;     // Zahl zwischen 350 und 850 => "Math.random()" gibt eine Zahl zwischen "0" und "1" aus
         this.speed = 0.15 + Math.random() * 0.25;
         
         this.animate();
