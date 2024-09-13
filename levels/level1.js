50f22901a8d39ebb71fa9ca4213c78a3358d0709
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-09-13 07:25:30 +0200

    refactored level1.js

--- levels/level1.js
+++ levels/level1.js
@@ -1,46 +1,59 @@
+const map1 = [
+    './img/5_background/layers/air.png',
+    './img/5_background/layers/3_third_layer/1.png',
+    './img/5_background/layers/2_second_layer/1.png',
+    './img/5_background/layers/1_first_layer/1.png'
+];
+
+const map2 = [
+    './img/5_background/layers/air.png',
+    './img/5_background/layers/3_third_layer/2.png',
+    './img/5_background/layers/2_second_layer/2.png',
+    './img/5_background/layers/1_first_layer/2.png'
+];
+
+const mapPositions = [-719, 0, 719, 719 * 2, 719 * 3];
+
 const level1 = new Level(
     [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new ChickenSmall(),
-        new ChickenSmall(),
-        new ChickenSmall(),
-        new ChickenSmall(),
-        new ChickenSmall(),
-        new Endboss()
+        ...createEnemies(5, 5)
     ],
     [
-        new Cloud(),
-        new Cloud(),
-        new Cloud(),
-        new Cloud()
+        ...createFirstBackgroundObject(4),
     ],
     [
-        new BackgroundObject('./img/5_background/layers/air.png', -719),
-        new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', -719),
-        new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', -719),
-        new BackgroundObject('./img/5_background/layers/1_first_layer/2.png', -719),
-        
-        // Alternative: via For-Schleife
-        new BackgroundObject('./img/5_background/layers/air.png', 0),
-        new BackgroundObject('./img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('./img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('./img/5_background/layers/1_first_layer/1.png', 0),
-        new BackgroundObject('./img/5_background/layers/air.png', 719),
-        new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', 719),
-        new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', 719),
-        new BackgroundObject('./img/5_background/layers/1_first_layer/2.png', 719),
-
-        new BackgroundObject('./img/5_background/layers/air.png', 719*2),
-        new BackgroundObject('./img/5_background/layers/3_third_layer/1.png', 719*2),
-        new BackgroundObject('./img/5_background/layers/2_second_layer/1.png', 719*2),
-        new BackgroundObject('./img/5_background/layers/1_first_layer/1.png', 719*2),
-        new BackgroundObject('./img/5_background/layers/air.png', 719*3),
-        new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', 719*3),
-        new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', 719*3),
-        new BackgroundObject('./img/5_background/layers/1_first_layer/2.png', 719*3)
+        ...createMaps([map1, map2], mapPositions)
     ]
 );
+
+function createEnemies(firstEnemyCount, secondEnemyCount) {
+    let enemies = [];
+    for (let i = 0; i < firstEnemyCount; i++) {
+        enemies.push(new Chicken());
+    }
+    for (let i = 0; i < secondEnemyCount; i++) {
+        enemies.push(new ChickenSmall());
+    }
+    enemies.push(new Endboss());
+
+    return enemies;
+}
+
+function createFirstBackgroundObject(number) {
+    let firstBackgroundObject = [];
+    for (let i = 0; i < number; i++) {
+        firstBackgroundObject.push(new Cloud());
+    }
+    return firstBackgroundObject;
+}
+
+function createMaps(mapGroup, mapPositions) {
+    let backgroundObjects = [];
+    for (let i = 0; i < mapPositions.length; i++) {
+        let currentMap = mapGroup[i % mapGroup.length];
+        for (let j = 0; j < currentMap.length; j++) {
+            backgroundObjects.push(new BackgroundObject(currentMap[j], mapPositions[i]));
+        }
+    }
+    return backgroundObjects;
+}
