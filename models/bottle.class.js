87172dd31913ed89fb36262c682067eb13697b91
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-09-20 17:53:56 +0200

    image of a bottle is picked randomly

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -11,7 +11,9 @@ class Bottle extends CollectableObject {
     constructor(x) {
         super();
         this.x = 350 + Math.random() * 1500;     // Zahl zwischen 350 und 1850 => "Math.random()" gibt eine Zahl zwischen "0" und "1" aus
-        this.loadImage('./img/6_salsa_bottle/1_salsa_bottle_on_ground.png'); // Bilder müssen noch ständig gewechselt werden
+        
+        let randomImage = this.IMAGES[Math.floor(Math.random() * this.IMAGES.length)];
+        this.loadImage(randomImage);
     }
 
     // Überschreibt die onCollect Methode, um Flaschen zur Anzahl der Wurfobjekte hinzuzufügen.
