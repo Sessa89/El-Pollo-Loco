99fb2150b5f18402a25804a0c545ac13b6449aeb
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-09-20 06:18:41 +0200

    refactored level.class.js and level1.js

--- levels/level1.js
+++ levels/level1.js
@@ -15,62 +15,118 @@ const map2 = [
 const mapPositions = [-719, 0, 719, 719 * 2, 719 * 3];
 
 const level1 = new Level(
-    [...createEnemies(5, 5)],
-    [...createBackgroundObjects(4)],
-    [...createMaps([map1, map2], mapPositions)],
-    [...createCollectableObjects(10)]
+    [...createEnemies()],
+    [...createBackgroundObjects()],
+    [...createCollectableObjects()],
+    [...createMaps([map1, map2], mapPositions)]
 );
 
-function createEnemies(firstEnemyCount, secondEnemyCount) {
+function createEnemies() {
     let enemies = [];
-    
-    for (let i = 0; i < firstEnemyCount; i++) {
-        enemies.push(new Chicken());
-    }
-    
-    for (let i = 0; i < secondEnemyCount; i++) {
-        enemies.push(new ChickenSmall());
-    }
 
-    enemies.push(new Endboss());
+    enemies.push(...createFirstEnemyType(5));   // creating x enemies of the first type (x = count)
+    enemies.push(...createSecondEnemyType(5));  // creating x enemies of the second type (x = count)
+    enemies.push(new Endboss());                // creating one endboss
 
     return enemies;
 }
 
-function createBackgroundObjects(firstBackgroundObjectCount) {
-    let backgroundObjects = [];
-    
-    for (let i = 0; i < firstBackgroundObjectCount; i++) {
-        backgroundObjects.push(new Cloud());
+function createFirstEnemyType(count) {
+    let enemies = [];
+
+    for (let i = 0; i < count; i++) {
+        enemies.push(new Chicken());
     }
-    
+
+    return enemies;
+}
+
+function createSecondEnemyType(count) {
+    let enemies = [];
+
+    for (let i = 0; i < count; i++) {
+        enemies.push(new ChickenSmall());
+    }
+
+    return enemies;
+}
+
+function createBackgroundObjects() {
+    let backgroundObjects = [];
+
+    backgroundObjects.push(...createFirstBackgroundObject(4));  // creating x background objects of the first type (x = count)
+
     return backgroundObjects;
+}
+
+function createFirstBackgroundObject(count) {
+    let backgroundObject = [];
+
+    for (let i = 0; i < count; i++) {
+        backgroundObject.push(new Cloud());
+    }
+
+    return backgroundObject;
 }
 
 function createMaps(mapGroup, mapPositions) {
     let maps = [];
-    
+
     for (let i = 0; i < mapPositions.length; i++) {
         let currentMap = mapGroup[i % mapGroup.length];
-        
+
         for (let j = 0; j < currentMap.length; j++) {
             maps.push(new BackgroundObject(currentMap[j], mapPositions[i]));
         }
     }
-    
+
     return maps;
 }
 
-function createCollectableObjects(firstCollectableObjectCount) {
+function createCollectableObjects() {
     let collectables = [];
 
-    for (let i = 0; i < firstCollectableObjectCount; i++) {
-        collectables.push(new Bottle());
-    }
+    collectables.push(...createBottles(10));            // creating x bottles (x = count)
 
-    collectables.push(new Coin(300, 100));
-    collectables.push(new Coin(500, 150));
-    collectables.push(new Coin(800, 200));
+    collectables.push(...createCoinRow(400, 100, 3));   // creating a row of coins (startX, y, count)
+    collectables.push(...createCoinRow(1800, 300, 3));  // creating a row of coins (startX, y, count)
+
+    collectables.push(...createCoinParabola(1200, 200, 5, 300, 150, 0.5));  // creating a parabolic curve of coins (startX, startY, count, width, height, flatness)
 
     return collectables;
+}
+
+function createBottles(count) {
+    let bottles = [];
+
+    for (let i = 0; i < count; i++) {
+        bottles.push(new Bottle());
+    }
+
+    return bottles;
+}
+
+function createCoinRow(startX, y, count) {
+    let coins = [];
+
+    for (let i = 0; i < count; i++) {
+        let xPosition = startX + i * 50;        // coins in a distance of 50px
+        coins.push(new Coin(xPosition, y));
+    }
+
+    return coins;
+}
+
+function createCoinParabola(startX, startY, count, width, height, flatness = 1) {
+    let coins = [];
+    let midPoint = Math.floor(count / 2);
+
+    for (let i = 0; i < count; i++) {
+        let xPosition = startX + (i * width / count);  // x-position for an even distribution
+        let yPosition = startY + Math.pow(i - midPoint, 2) * (height / Math.pow(midPoint, 2)) * flatness;   // y-position for a mirrored, flat parabola
+
+        coins.push(new Coin(xPosition, yPosition));
+    }
+
+    return coins;
 }
--- models/level.class.js
+++ models/level.class.js
@@ -5,10 +5,10 @@ class Level {
     collectableObjects;
     level_end_x = 2200;
 
-    constructor(e, c, b, co) {
+    constructor(e, c, co, b) {
         this.enemies = e;
         this.clouds = c;
-        this.backgroundObjects = b;
         this.collectableObjects = co;
+        this.backgroundObjects = b;
     }
 }
