78997de4de25ddcaef828181ccb38c7067be6458
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-09-27 06:09:01 +0200

    refactored endboss.class.js and replaced right images for walking animation

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -9,14 +9,10 @@ class Endboss extends MoveableObject {
     distanceToCharacter;
 
     IMAGES_WALKING = [
-        './img/4_enemie_boss_chicken/2_alert/G5.png',
-        './img/4_enemie_boss_chicken/2_alert/G6.png',
-        './img/4_enemie_boss_chicken/2_alert/G7.png',
-        './img/4_enemie_boss_chicken/2_alert/G8.png',
-        './img/4_enemie_boss_chicken/2_alert/G9.png',
-        './img/4_enemie_boss_chicken/2_alert/G10.png',
-        './img/4_enemie_boss_chicken/2_alert/G11.png',
-        './img/4_enemie_boss_chicken/2_alert/G12.png'
+        './img/4_enemie_boss_chicken/1_walk/G1.png',
+        './img/4_enemie_boss_chicken/1_walk/G2.png',
+        './img/4_enemie_boss_chicken/1_walk/G3.png',
+        './img/4_enemie_boss_chicken/1_walk/G4.png'
     ];
 
     IMAGES_ALERT = [
@@ -95,7 +91,7 @@ class Endboss extends MoveableObject {
             this.alert_sound.play();
             this.playAnimation(this.IMAGES_ALERT);
             console.log('Endboss has been alerted!');
-        } else if (this.CharachterIsInFieldOfVision()) {
+        } else if (this.CharacterIsInFieldOfVision()) {
             this.chaseCharacter();
             this.playAnimation(this.IMAGES_WALKING);
         } else if (this.CharacterIsInRangeToAttack()) {
@@ -103,13 +99,10 @@ class Endboss extends MoveableObject {
         } else if (this.CharacterIsOutOfSight()) {
             this.returnToStart();    
             this.playAnimation(this.IMAGES_WALKING);
-        } else {
-            // 
-            this.playAnimation(this.IMAGES_WALKING);
         }
     }
 
-    CharachterIsInFieldOfVision() {
+    CharacterIsInFieldOfVision() {
         return this.distanceToCharacter <= 500;
     }
 
